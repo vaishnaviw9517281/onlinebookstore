06cf4bdb650e71e1d0eff5ed71d9774bacaca81d
Vaishnavi <vaishnavi.w9517281@gmail.com>
2022-11-26 11:25:40 +0000

    cors update

--- onlinebookstore/src/main/java/in/bushansirgur/onlinebookstore/config/RepositoryConfig.java
+++ onlinebookstore/src/main/java/in/bushansirgur/onlinebookstore/config/RepositoryConfig.java
@@ -23,10 +23,6 @@ public class RepositoryConfig implements RepositoryRestConfigurer{
 				.toArray(Class[]::new));
 		config.getCorsRegistry()
 				.addMapping("/**")
-				.allowedHeaders("Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
-                        "Access-Control-Request-Headers")
-				.allowedOrigins("http://3.93.31.194:4200")
-				.allowedOrigins("http://localhost:4200")
-				.allowedOrigins("http://0.0.0.0:4200");
+				.allowedOrigins("**");
 	}
 }
