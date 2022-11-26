edefff3fef26f32df9081ee875a143469fbdddb1
Vaishnavi <vaishnavi.w9517281@gmail.com>
2022-11-26 11:21:59 +0000

    updated with 0.0.0.0

--- onlinebookstore/src/main/java/in/bushansirgur/onlinebookstore/config/RepositoryConfig.java
+++ onlinebookstore/src/main/java/in/bushansirgur/onlinebookstore/config/RepositoryConfig.java
@@ -26,6 +26,7 @@ public class RepositoryConfig implements RepositoryRestConfigurer{
 				.allowedHeaders("Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
                         "Access-Control-Request-Headers")
 				.allowedOrigins("http://3.93.31.194:4200")
-				.allowedOrigins("http://localhost:4200");
+				.allowedOrigins("http://localhost:4200")
+				.allowedOrigins("http://0.0.0.0:4200");
 	}
 }
